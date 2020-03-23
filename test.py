20292a962eb60f8c51ec399952c2e665e04179c2
LucaJaich <jaichencol@gmail.com>
2020-03-23 20:54:36 -0300

    adding db conn

--- test.py
+++ test.py
@@ -1,15 +1,21 @@
 from flask import Flask, request
 from flask_cors import CORS
 import json
+import pymysql
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route("/test", methods=["POST"])
 def manage_request():
+    db = pymysql.connect("35.196.168.71", "jPolo", "megapolo", "ConvidAr")
+    cursor = db.cursor()
     req = json.loads(request.data)
-    num = int(req["num"]) + 1
-    res = {"num": num}
-    return json.dumps(res)
+    table = req["table"]
+    sql = "SELECT * FROM {}".format(table)
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    
+    return json.dumps({"res": results[0][3]})
 
-app.run()
+app.run()       
